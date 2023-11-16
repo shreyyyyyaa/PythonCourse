6f3e3618e4c1428416574132d137d6ae199d28c9
Shreya <shreyyyyyaa@gmail.com>
2023-11-16 18:52:01 +0530

    Lesson 1

---
+++ module-1/L1/app.py
@@ -0,0 +1,8 @@
+
+print("""
+**********************************************************************
+-----------------Welcome to the Inventory System!---------------------
+About us: This system helps you manage your products efficiently.
+Contact us: Shreya - 9999999999
+**********************************************************************
+Let's get started!""")
